15845e42b8a8245c597d2d4958e44633be855696
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-09-17 13:15:55 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+git checkout .
+git pull --force
 sed -i '' -e "s/IMG_PARA_SUBSTITUIR/didox\/k8s_ecommerce:$TAG/" deployment-ecommerce-pipeline.yml
 kubectl apply -f deployment-ecommerce-pipeline.yml
